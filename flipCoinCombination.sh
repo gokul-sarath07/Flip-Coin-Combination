4cd08eaa394691fa48b0df9ab1cececae1759da7
gokul-sarath07 <gokul510.s@gmail.com>
2020-08-05 18:56:27 +0530

    Percentage of Heads and Tails

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 echo "Coin Flip Combination"
-check=$(( RANDOM%2 ))
-if [[ $check -eq 1 ]]
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
+declare -A dict
+ct=0
+heads=0
+tails=0
+for i in {1..20}
+do
+	check=$(( RANDOM%2 ))
+	if [[ $check -eq 1 ]]
+	then
+		dict[$ct]="Heads"
+		heads=$(( $heads + 1 ))
+	else
+		dict[$ct]="Tails"
+		tails=$(( $tails + 1 ))
+	fi
+	ct=$(( $ct + 1))
+done
+perHead=$(( $heads * 100 ))
+perTail=$(( $tails * 100 ))
+echo "Head Percentage: $(( perHead/20 ))%"
+echo "Tail Percentage: $(( perTail/20 ))%"
